6e7ae0916b9313202708b1eeda0142b7d1686aad
Jake McLeod <jake@websiteglobal.com.au>
2020-02-12 13:30:39 +1000

    latest commit

--- storage/cms/twig/d8/d8ea2a0fdb3d5ebe86dd4fb939615aef66f27ce5d73e35dce03f52d92664c47b.php
+++ storage/cms/twig/d8/d8ea2a0fdb3d5ebe86dd4fb939615aef66f27ce5d73e35dce03f52d92664c47b.php
@@ -52,70 +52,100 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
             // line 9
             echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "doctor_name", [], "any", false, false, false, 9), "html", null, true);
             echo "</h2>
-        <p><em>";
-            // line 10
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "practice_name", [], "any", false, false, false, 10), "html", null, true);
-            echo "</em></p>
-    
         ";
-            // line 12
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "address_1", [], "any", false, false, false, 12), "html", null, true);
-            if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "address_2", [], "any", false, false, false, 12)) {
+            // line 10
+            if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "practice_name", [], "any", false, false, false, 10)) {
+                // line 11
+                echo "            <p><em>";
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "practice_name", [], "any", false, false, false, 11), "html", null, true);
+                echo "</em></p>
+        ";
+            }
+            // line 13
+            echo "    
+        ";
+            // line 14
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "address_1", [], "any", false, false, false, 14), "html", null, true);
+            if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "address_2", [], "any", false, false, false, 14)) {
                 echo " / ";
-                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "address_2", [], "any", false, false, false, 12), "html", null, true);
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "address_2", [], "any", false, false, false, 14), "html", null, true);
             }
             echo ",<br />
         ";
-            // line 13
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "city", [], "any", false, false, false, 13), "html", null, true);
+            // line 15
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "city", [], "any", false, false, false, 15), "html", null, true);
             echo ", ";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "country", [], "any", false, false, false, 13), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "country", [], "any", false, false, false, 15), "html", null, true);
             echo ", ";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "state", [], "any", false, false, false, 13), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "state", [], "any", false, false, false, 15), "html", null, true);
             echo ",";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "postcode", [], "any", false, false, false, 13), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "postcode", [], "any", false, false, false, 15), "html", null, true);
             echo "
         
         ";
-            // line 15
-            if ((twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "phone", [], "any", false, false, false, 15) || twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "fax", [], "any", false, false, false, 15))) {
-                // line 16
+            // line 17
+            if ((twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "phone", [], "any", false, false, false, 17) || twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "fax", [], "any", false, false, false, 17))) {
+                // line 18
                 echo "            <ul class=\"directory detailed\">
                 <li>
                     <ul>
                     ";
-                // line 19
-                if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "phone", [], "any", false, false, false, 19)) {
-                    // line 20
+                // line 21
+                if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "phone", [], "any", false, false, false, 21)) {
+                    // line 22
                     echo "                        <li class=\"icon phone\">";
-                    echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "phone", [], "any", false, false, false, 20), "html", null, true);
+                    echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "phone", [], "any", false, false, false, 22), "html", null, true);
                     echo "</li>
                     ";
                 }
-                // line 21
+                // line 23
                 echo "    
                     ";
-                // line 22
-                if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "fax", [], "any", false, false, false, 22)) {
-                    // line 23
+                // line 24
+                if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "fax", [], "any", false, false, false, 24)) {
+                    // line 25
                     echo "                        <li class=\"icon fax show{tag_show fax number}\">";
-                    echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "fax", [], "any", false, false, false, 23), "html", null, true);
+                    echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "fax", [], "any", false, false, false, 25), "html", null, true);
                     echo "</li>
                     ";
                 }
-                // line 25
+                // line 27
                 echo "                    </ul>
                 </li>
             </ul>
         ";
             }
-            // line 29
-            echo "        <div class=\"linkButtons row pt-3\">
-            <div class=\"col-xs-6 email show{tag_show email}\"><a href=\"mailto:{tag_email}\" class=\"button\">Email</a></div>
-            <div class=\"col-xs-6 website show{tag_show webaite}\"><a href=\"http://{tag_website (ie www.)}\" target=\"_blank\" class=\"button\">Website</a></div>
-        </div>
-
+            // line 31
+            echo "        ";
+            if ((twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "email", [], "any", false, false, false, 31) || twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "website", [], "any", false, false, false, 31))) {
+                // line 32
+                echo "            <div class=\"linkButtons row pt-3\">
+            ";
+                // line 33
+                if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "email", [], "any", false, false, false, 33)) {
+                    // line 34
+                    echo "                <div class=\"col-xs-6 email\"><a href=\"mailto:";
+                    echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "email", [], "any", false, false, false, 34), "html", null, true);
+                    echo "\" class=\"button\">Email</a></div>
+            ";
+                }
+                // line 36
+                echo "            ";
+                if (twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "website", [], "any", false, false, false, 36)) {
+                    // line 37
+                    echo "                <div class=\"col-xs-6 website\"><a href=\"";
+                    echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["record"] ?? null), "website", [], "any", false, false, false, 37), "html", null, true);
+                    echo "\" target=\"_blank\" class=\"button\">Website</a></div>
+            ";
+                }
+                // line 39
+                echo "            </div>
+        ";
+            }
+            // line 41
+            echo "
         <div class=\"goBack hidden-xs\" style=\"margin-top: 20px;\"><p>{module_goback,Go Back}</p></div>
+        <button onclick=\"goBack()\">Go Back</button>
 
     </div>
     
@@ -123,7 +153,10 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
         <p>{module_webappsmap id=\"33152\" filter=\"item\" googlekey=\"AIzaSyB_pPFF55x8Z-b1K9PjsrXX0BM0vXyKGHc\" itemId=\"{tag_itemid}\"}</p>
     </div>
 
-    <div class=\"col-xs-12 goBack mobile hidden-sm hidden-md hidden-lg\" style=\"margin-top: 20px;\"><p>{module_goback,Go Back}</p></div>
+    <div class=\"col-xs-12 goBack mobile hidden-sm hidden-md hidden-lg\" style=\"margin-top: 20px;\"><p><a href=\"";
+            // line 51
+            echo twig_escape_filter($this->env, ($context["go_back"] ?? null), "html", null, true);
+            echo "\">Go Back</a></p></div>
 
 </div>
     
@@ -133,7 +166,7 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
     
 ";
         } else {
-            // line 51
+            // line 60
             echo "    ";
             echo twig_escape_filter($this->env, ($context["notFoundMessage"] ?? null), "html", null, true);
             echo "
@@ -153,7 +186,7 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
 
     public function getDebugInfo()
     {
-        return array (  137 => 51,  113 => 29,  107 => 25,  101 => 23,  99 => 22,  96 => 21,  90 => 20,  88 => 19,  83 => 16,  81 => 15,  70 => 13,  62 => 12,  57 => 10,  53 => 9,  46 => 5,  43 => 4,  41 => 3,  39 => 2,  37 => 1,);
+        return array (  170 => 60,  158 => 51,  146 => 41,  142 => 39,  136 => 37,  133 => 36,  127 => 34,  125 => 33,  122 => 32,  119 => 31,  113 => 27,  107 => 25,  105 => 24,  102 => 23,  96 => 22,  94 => 21,  89 => 18,  87 => 17,  76 => 15,  68 => 14,  65 => 13,  59 => 11,  57 => 10,  53 => 9,  46 => 5,  43 => 4,  41 => 3,  39 => 2,  37 => 1,);
     }
 
     public function getSourceContext()
@@ -167,7 +200,9 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
 
     <div class=\"col-sm-4\">
         <h2>{{ record.doctor_name }}</h2>
-        <p><em>{{ record.practice_name }}</em></p>
+        {% if record.practice_name %}
+            <p><em>{{ record.practice_name }}</em></p>
+        {% endif %}
     
         {{ record.address_1 }}{% if record.address_2 %} / {{ record.address_2 }}{% endif %},<br />
         {{ record.city }}, {{ record.country }}, {{ record.state }},{{ record.postcode }}
@@ -186,12 +221,19 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
                 </li>
             </ul>
         {% endif %}
-        <div class=\"linkButtons row pt-3\">
-            <div class=\"col-xs-6 email show{tag_show email}\"><a href=\"mailto:{tag_email}\" class=\"button\">Email</a></div>
-            <div class=\"col-xs-6 website show{tag_show webaite}\"><a href=\"http://{tag_website (ie www.)}\" target=\"_blank\" class=\"button\">Website</a></div>
-        </div>
+        {% if (record.email or record.website) %}
+            <div class=\"linkButtons row pt-3\">
+            {% if record.email %}
+                <div class=\"col-xs-6 email\"><a href=\"mailto:{{ record.email }}\" class=\"button\">Email</a></div>
+            {% endif %}
+            {% if record.website %}
+                <div class=\"col-xs-6 website\"><a href=\"{{ record.website }}\" target=\"_blank\" class=\"button\">Website</a></div>
+            {% endif %}
+            </div>
+        {% endif %}
 
         <div class=\"goBack hidden-xs\" style=\"margin-top: 20px;\"><p>{module_goback,Go Back}</p></div>
+        <button onclick=\"goBack()\">Go Back</button>
 
     </div>
     
@@ -199,7 +241,7 @@ class __TwigTemplate_a3ea3f95064cf7a06abb7064f298ebc66d51c0c45f881615343f291a4c0
         <p>{module_webappsmap id=\"33152\" filter=\"item\" googlekey=\"AIzaSyB_pPFF55x8Z-b1K9PjsrXX0BM0vXyKGHc\" itemId=\"{tag_itemid}\"}</p>
     </div>
 
-    <div class=\"col-xs-12 goBack mobile hidden-sm hidden-md hidden-lg\" style=\"margin-top: 20px;\"><p>{module_goback,Go Back}</p></div>
+    <div class=\"col-xs-12 goBack mobile hidden-sm hidden-md hidden-lg\" style=\"margin-top: 20px;\"><p><a href=\"{{ go_back }}\">Go Back</a></p></div>
 
 </div>
     
